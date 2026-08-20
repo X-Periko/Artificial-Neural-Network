6c73941831b391a69fe1c6028c687cfcd580b9db
Pablo Martínez Prado <138979989+X-Periko@users.noreply.github.com>
2026-08-20 20:03:09 +0000

    Individual neuron nearly finished

--- Network_classes.py
+++ Network_classes.py
@@ -1,3 +1,5 @@
+import random as r
+
 class Neuron:
     def __init__(self, weights, bias):
         self.weights = weights
@@ -10,28 +12,59 @@ class Neuron:
         
         return self.prediction
 
-    def train(self, inputs, exp_out, learning_rate, epochs):
+    def train(self, inputs: list, exp_out:list, learning_rate, epochs:int):
+        needed_weights = len(inputs[0])
+        for input in inputs:
+            if len(input) != needed_weights:
+                print("[!] Inputs with different lenghts were provided")
+                raise Exception
+        if needed_weights != len(self.weights):
+            print("[!] Amount of inputs didn't match the amount of weights")
+            raise Exception
+        input_i = 0
+        last_error = []
         for i in range(epochs):
-            self.pred = self.output(inputs=inputs)
+            input_i += 1
+
+            if input_i == len(inputs):
+                input_i = 0
+            self.pred = self.output(inputs=inputs[input_i])
+            if self.pred == exp_out[input_i]:
+                print("=========================================================================================\n")
+                print(f"Expected output achieved in iteration {i}. Training concluded with weihgts {self.weights}.\n")
+                print("=========================================================================================\n")
+                break
             print(f"Pred: {self.pred}")
-            error = (exp_out - self.pred)
+            error = abs(exp_out[input_i] - self.pred)
+            if error in last_error:
+                print("=========================================================================================\n")
+                print(f"[!] Minimum error {error} achieved at iteration {i}\n")
+                print("=========================================================================================\n")
+                break
+            last_error.append(error)
             self.adjust_weights(loss=error, learning_rate=learning_rate)
             print(f"Iteration {i}: Weights: {self.weights}, Error: {error}")
-
+            
     def adjust_weights(self, loss, learning_rate):
         for i, w in enumerate(self.weights, start=0):
             grad = 2*(loss)*w
             print(f"Grad: {grad}")
-            adjusted_w = w + (grad * learning_rate)
+            adjusted_w = w - (grad * learning_rate)
             self.weights[i] = adjusted_w
 
 class Layer:
-    def __init__(self, neurons, inputs):
+    def __init__(self, neurons, inputs, parameters, bias, size):
         self.neurons = neurons
+        self.parameters = parameters
+        self.bias = bias
         self.inputs = inputs
+        self.size = size
+
+        for id in self.size:
+            pass
 
 class Network:
     pass
 
-N1 = Neuron(weights=[1, 4],bias=0.5)
-N1.train(inputs=[3, 1], exp_out=10, learning_rate=0.012, epochs=100)
+N1 = Neuron(weights=[3, 2],bias=0.5)
+N1.train(inputs=[[4, 5], [6, 10]], exp_out=[9, 18], learning_rate=0.012, epochs=100)
